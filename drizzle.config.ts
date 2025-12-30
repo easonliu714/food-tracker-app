88ce896e8b8c09f6fe20a082c3d2dd1752fb3075
Liu Chia Yi <eason714@gmail.com>
2025-12-30 19:23:33 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,11 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is required to run drizzle commands");
 }
 
+import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
-  dialect: "mysql",
-  dbCredentials: {
-    url: connectionString,
-  },
+  dialect: "sqlite", // [修改] 改為 sqlite
+  driver: "expo", // [新增] 指定為 expo driver
 });
