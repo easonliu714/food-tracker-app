81d9560d84a4af823f81c190c61fc8a61f640f21
Liu Chia Yi <eason714@gmail.com>
2026-01-14 10:31:36 +0000

    update v1.0.15

--- app.config.ts
+++ app.config.ts
@@ -17,7 +17,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.14.5", // 建議進版，確保安裝時視為新版本
+  version: "1.0.15", // 建議進版，確保安裝時視為新版本
   orientation: "portrait",
   scheme: "foodtracker", 
 
